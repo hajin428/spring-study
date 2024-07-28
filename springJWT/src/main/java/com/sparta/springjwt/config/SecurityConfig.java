2f3f65fdd7cc4eab3da48d6060a160a3de37e228
hajin428 <amm_321@naver.com>
2024-07-29 01:32:59 +0900

    config 클래스 기본 세팅

--- springJWT/src/main/java/com/sparta/springjwt/config/SecurityConfig.java
+++ springJWT/src/main/java/com/sparta/springjwt/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,10 +13,16 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        //비밀번호를 암호화하기 위한 메서드
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
     //인자로 httpSecurity를 받고 예외 처리 해줌
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        return http.build();
+        //csrf 보호와 기본적인 인증 방식 비활성화(disable)
 
         //csrf disable
         http
@@ -28,12 +36,27 @@ public class SecurityConfig {
         http
                 .httpBasic((auth) -> auth.disable());
 
+
+
         //경로별 인가 작업
         http
+                //authorizeHttpRequests:  http 요청에 대한 인증
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/login", "/", "/join").permitAll()
-                        .requestMatchers("/admin").hasRole("ADMIN")
-                        .anyRequest().authenticated());
+                        .requestMatchers("/login", "/", "/join").permitAll() //특정 경로에 대해서는 인증 생략
+                        .requestMatchers("/admin").hasRole("ADMIN") //"/admin"경로에 대해서는 ADMIN 사용자만 접근 가능
+                        .anyRequest().authenticated()); //나머지 모든 요청에 대해서는 인증 요구
 
+        //세션 설정
+        http
+                .sessionManagement((session) -> session //sessionManagment : 세션 관리
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)); //세션을 Stateless 상태로 관리
+
+        /*
+        세션을 Stateless 상태로 관리하는 이유
+        Stateless 방식은 서버가 클라이언트의 이전 요청을 기억하지 않고, 각 요청을 독립적으로 처리하는 방식
+        RESTful Api와 호환됨
+         */
+
+        return http.build();
     }
 }
